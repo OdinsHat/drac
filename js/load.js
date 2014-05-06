4f6a461678ebb98a407047c1e518641aaff9eaac
Doug Bromley <doug.bromley@gmail.com>
2014-05-06 16:22:12 +0100

    Adding bulk of preload code to loader stage

--- js/load.js
+++ js/load.js
@@ -2,6 +2,26 @@ var Drac = {};
 Drac.Boot = function(game) {};
 Drac.Boot.prototype = {
     preload: function() {
-        this.load.image
+        this.game.stage.backgroundColor = '#000000';
+
+        // Load characters
+        this.load.spritesheet('you', 'assets/yousheet.png');
+        this.load.spritesheet('drac', 'assets/dracsheet.png');
+
+        //Load UI parts
+        this.load.image('button-start', 'assets/button-start.png');
+        this.load.image('button-credits', 'assets/button-credits.png');
+
+        //Load environment
+        this.load.image('background', 'assets/background.jpg');
+        this.load.image('ledge1', 'assets/ledge1.png');
+        this.load.image('block1', 'assets/block1.png');
+        this.load.image('fire', 'assets/CampFireFinished.png');
+
+        //Load music
+        this.load.audio('ghosts', ['assets/ghosts.ogg']);
+    },
+    create: function(){
+        this.game.state.start('Menu');
     }
 }
