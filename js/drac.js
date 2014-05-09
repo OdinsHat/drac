b2683ce0aa4b1f7fce4df72b08f164f5e4b9402a
Doug Bromley <doug.bromley@gmail.com>
2014-05-09 11:15:47 +0100

    drac.js file for initialising Phaser game object and adding states.
    All done inside self executing function.

---
+++ js/drac.js
@@ -0,0 +1,8 @@
+(function(){
+    var game = new Phaser.Game(640, 480, Phaser.AUTO, 'drac-game');
+
+    game.state.add('Loader', Drac.Loader);
+    game.state.add('Menu', Drac.Menu);
+    game.state.add('Credits', Drac.Credits);
+    game.state.add('Game', Drac.MainGame)
+})();
