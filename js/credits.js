894c2eda77a335784e222981d2ef4b0d23f10261
Doug Bromley <doug.bromley@gmail.com>
2014-05-09 11:49:11 +0100

    Added skeleton of credits page

--- js/credits.js
+++ js/credits.js
@@ -1,1 +1,16 @@
 Drac.Credits = function(game) {};
+Drac.Credits.prototype = {
+    /**
+     * Display game credits with fire plugin.
+     *
+     * Can start game from here using space as on main menu.
+     *
+     * @return undefined
+     */
+    create: function() {
+
+    },
+    start: function() {
+        this.game.state.start('MainGame');
+    }
+}
