ad17148fad574a2f6a4a66d04337a55d4caedcc7
Doug Bromley <doug.bromley@gmail.com>
2014-05-09 11:48:37 +0100

    Laying out main game.js file

--- js/game.js
+++ js/game.js
@@ -1,6 +1,24 @@
-Drac.MainGame = function(game) {};
-Drac.MainGame.prototype = {
-    preload: function() {
-        this.load.image
+Drac.Game = function(game) {
+    score = 0;
+    lives = 0;
+};
+
+/**
+ * No preload function required here as that is handled in the Drac.Loader state.
+ *
+ * @type {Object}
+ */
+Drac.Game.prototype = {
+    create: function() {
+
+    },
+    update: function() {
+
+    },
+    touchDrac: function() {
+
+    },
+    hitDrac: function() {
+
     }
-}
+};
