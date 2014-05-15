28c6b71ad99bd215e0428a175c6098bc41f154c3
Doug Bromley <doug.bromley@gmail.com>
2014-05-15 18:02:21 +0100

    Begun adding credits of assets so far

--- js/credits.js
+++ js/credits.js
@@ -8,7 +8,20 @@ Drac.Credits.prototype = {
      * @return undefined
      */
     create: function() {
+        this.game.stage.setBackgroundColor(0x2d2d2d);
 
+        var creditsText = this.game.add.text(
+            this.game.world.centerX,
+            this.game.world.centerY,
+            "Developer: Doug Bromley\nHero Character Art: Stephen Challener & Johannes Sjölund\nOther Art: To follow",
+            {
+                font: "20px Arial",
+                fill: "#000000",
+                strokeThickness: 1,
+                align: 'left'
+            }
+        );
+        creditsText.anchor.set(0.5);
     },
     start: function() {
         this.game.state.start('Game');
