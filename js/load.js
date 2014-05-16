5210f6d3eefcc7eea39f7ac186cbfbc1d45cdfe9
Doug Bromley <doug.bromley@gmail.com>
2014-05-16 22:28:00 +0100

    Commenting out unused code in preload for now
    So we can get to menu for now and master will be
    workable from Github. All future work will be done
    on dev branch and merged as and when.

--- js/load.js
+++ js/load.js
@@ -14,13 +14,13 @@ Drac.Loader.prototype = {
         this.load.spritesheet('btn-credits', 'assets/btn-credits-sprite.png', 220, 58);
 
         //Load environment
-        this.load.image('background', 'assets/background.jpg');
-        this.load.image('ledge1', 'assets/ledge1.png');
-        this.load.image('block1', 'assets/block1.png');
-        this.load.image('fire', 'assets/CampFireFinished.png');
+        //this.load.image('background', 'assets/background.jpg');
+        //this.load.image('ledge1', 'assets/ledge1.png');
+        //this.load.image('block1', 'assets/block1.png');
+        //this.load.image('fire', 'assets/CampFireFinished.png');
 
         //Load music
-        this.load.audio('ghosts', ['assets/ghosts.ogg']);
+        //this.load.audio('ghosts', ['assets/ghosts.ogg']);
     },
     create: function(){
         this.game.state.start('Menu');
