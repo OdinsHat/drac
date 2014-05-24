048b9e02ee3381849bed3fb58ba01804f9af4db3
Doug Bromley <doug.bromley@gmail.com>
2014-05-24 12:34:36 +0100

    Adding floor as simple rect - finding graphics assets is tough

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ Drac.Game = function(game) {
  */
 Drac.Game.prototype = {
     create: function() {
-
+        this.createFloor();
     },
     update: function() {
 
@@ -23,5 +23,15 @@ Drac.Game.prototype = {
     },
     quitGame: function(){
         this.state.start('Menu');
+    },
+    createFloor: function(){
+        // Init vector shape
+        this.ground = this.game.add.graphics(0, 0);
+        this.ground.lineStyle(2, 0x663300, 1);
+        this.ground.beginFill(0x331900, 1);
+
+        // Draw shape
+        this.ground.drawRect(-2, this.game.world.height-20, this.game.world.width+20, 20);
+        this.ground.immovable = true;
     }
 };
