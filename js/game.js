c3da3ac402b18ce74b71a24cb5509c0122b0785e
Doug Bromley <doug.bromley@gmail.com>
2014-05-24 17:37:33 +0100

    Set up all animations for future use

--- js/game.js
+++ js/game.js
@@ -15,8 +15,23 @@ Drac.Game.prototype = {
         this.player = this.game.add.sprite(32, this.game.world.height-160, 'you');
         this.game.physics.arcade.enable(this.player);
 
-        this.player.animations.add('left', [117, 118, 119, 120, 121, 122, 123, 124, 125], 10, true);
-        this.player.animations.add('right', [143, 144, 145, 146, 147, 148, 149, 150, 151], 10, true);
+        // Animations
+        this.player.animations.add('left', [117, 118, 119, 120, 121, 122, 123, 124, 125], 10, true); // Row J
+        this.player.animations.add('right', [143, 144, 145, 146, 147, 148, 149, 150, 151], 10, true); // Row L
+
+        this.player.animations.add('cast-small-left', [65, 66, 67, 68, 69, 70, 71, 72], 10, true); // Row F
+        this.player.animations.add('cast-small-right', [91, 92, 93, 94, 95, 96, 97, 98], 10, true); // Row H
+
+        this.player.animations.add('cast-large-left', [221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233]); // Row R
+        this.player.animations.add('cast-large-right', [247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259]); // Row T0
+
+        this.player.animations.add('castout-right', [39, 40, 41, 42, 43, 44, 45]); // Row D
+        this.player.animations.add('castout-left', [13, 14, 15, 16, 17, 18, 19]); // Row B
+
+        this.player.animations.add('slash-left'), [169,170, 171, 172, 173, 174, 175]); //Row N
+        this.player.animations.add('slash-right'), [195, 196, 197, 198, 199, 200]); // Row P
+
+        this.player.animations.add('player-die', [260, 261, 262, 263, 264, 265]); // Row U
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
     },
